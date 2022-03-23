f362e4cbcc5d5f207a3f926df568347e38c472d0
haykhoxton <101787977+haykhoxton@users.noreply.github.com>
2022-03-23 18:02:12 +0400

    sales page

---
+++ src/views/salses/index.js
@@ -0,0 +1,15 @@
+// material-ui
+import { Typography } from '@mui/material';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+
+// ==============================|| SAMPLE PAGE ||============================== //
+
+const SamplePage = () => (
+    <MainCard title="Sales page">
+        <Typography variant="body2">sales page</Typography>
+    </MainCard>
+);
+
+export default SamplePage;
